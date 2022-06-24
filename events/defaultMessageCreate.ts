739527ed4511bfdd9ab9d1fd8ba0867867f4f661
Eight∞ <wangzndg@gmail.com>
2022-06-24 21:06:01 +0200

    feat: remove custom implementations for interactions

--- events/defaultMessageCreate.ts
+++
@@ -1,6 +0,0 @@
-import { Event } from "../utils/Event";
-
-export default new Event("messageCreate", (message, client) => {
-  if (message.author.bot) return;
-  client.processCommand(message);
-});
--- interactionEvents/slashInteractionCreate.ts
+++
@@ -1,7 +0,0 @@
-import { Event } from "../utils/Event";
-
-export default new Event("slashInteractionCreate", (interaction, client) => {
-  if (client.slashCommands.find((c) => c.name === interaction.data?.name)) {
-    client.processSlashCommand(interaction);
-  }
-});
